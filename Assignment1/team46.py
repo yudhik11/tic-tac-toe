406b82842dcbcbf8cc8b4a34ab51b357a41298f2
anveshc05 <anveshc05@gitlab.com>
2018-02-21 20:27:59 +0530

    IDS implemented

--- Assignment1/team46.py
+++ Assignment1/team46.py
@@ -1,46 +1,87 @@
+import sys
 import random
 import datetime
 import copy
 
 class Player46:
-
 	def __init__(self):
-        self.begin = 0
-        self.timer = 0
-		self.timelimit = datetime.timedelta(seconds=2.5)
-        self.depth = 4
-        self.cnt = 0
+		self.INF = int(1e9)
+		self.maxmove = False
+		self.timeLimit = datetime.timedelta(seconds = 2.5)
+		self.begin = 0
+		self.mark = 'x'
+		self.zobrist= []
+		self.transpositionTable = {}
+		self.index = 0
+		for i in range(0, 16):
+			self.zobrist.append([])
+			for j in range(0, 16):
+				self.zobrist[i].append([])
+				for k in range(0, 17):
+					self.zobrist[i][j].append([])
+					for l in range(0, 17):
+						self.zobrist[i][j][k].append([])
+						for m in range(0, 2):
+							self.zobrist[i][j][k][l].append(random.randint(0, 2**64))
 
-    def check_timer(self, exp):
-        if exp - self.begin > self.timelimit :
-        	return True
-        return False
+	def move(self, board, old_move, flag):
+		self.mark = flag
+		board_copy = copy.deepcopy(board)
+		curr = copy.deepcopy(old_move)
+		self.begin = datetime.datetime.utcnow()
+		ans = self.IDS(curr)
+		return ans
 
-    def minimax(self,board,old_move,alpha,beta,maxim,flag,depthi,depth,initial):
-    	if self.check_timer(datetime.datetime.utcnow()):
-    		return (0, (-1, -1))
-    	else:
-    		check_end = board.find_terminal_state()
-    		if check_end[1] == "WON":
-    			if check_end[0] == flag:
-    				return (10000, old_move)
-    			return (-10000, old_move)
+	def check_time(self):
+		if datetime.datetime.utcnow() - self.begin > self.timeLimit:
+			return True
+		return False
 
-    		if initial_depth == depth
+	def IDS(self, root):
+		guess = 0
+		for depth in range(1, 500):
+			self.transpositionTable = {}
+			guess, move = self.MTDF(root, guess, depth)
+			if self.check_time():
+				break
+			saved_move = move
+		return saved_move
 
+	def MTDF(self, root, guess, depth):
+		g = guess
+	    upperbound = self.INF
+	    lowerbound = -self.INF
+	    while lowerbound < upperbound
+	        if g == lowerbound:
+	        	beta = g + 1
+	        else:
+	        	beta = g
+	        self.index = 0
+	        self.maxmove = True
+	        g = AlphaBetaWithMemory(root, beta - 1, beta, d)
+	        if check_time():
+	        	retrun g, move
+	        if g < beta:
+	        	upperbound = g
+	        else:
+	        	lowerbound = g
+	    return g, move
 
-    def move(self ,board, old_move, flag):
-    	self.begin = datetime.datetime.utcnow()
-    	global initial_depth
-    	initial_depth = 0
-    	while self.check_timer(datetime.datetime.utcnow()) == False and self.initial_depth < 8:
-    		optimal = self.minimax(board, old_move, int(1e9) , -int(1e9), 1, flag, 0, (5,7))
-    		index = ()
-    		if optimal[1] != (-1,-1):
-    			index = optimal[1]
-    		initial_depth += 1
-    	return index
+	def calcZobristHash(self, root):
+		h = 0
+		bs = board.board_status
+		for i in range(0, 16):
+			for j in range(0, 16):
+				if bs[i][j] == 'x':
+					h = h^self.zobrist[i][j][root[0]+1][root[1]+1][0]
+				elif bs[i][j] == 'o':
+					h = h^self.zobrist[i][j][root[0]+1][root[1]+1][1]
+		return h
 
+	def cantorPairing(self, a, b):
+		return (a + b)*(a + b + 1)/2 + b
 
-
-
+	def AlphaBetaWithMemory(self, root, alpha, beta, depth):
+		board_hash = self.calcZobristHash(root)
+		loweridx = self.cantorPairing(board_hash, 1)
+		upperidx = self.cantorPairing(board_hash, 2)
