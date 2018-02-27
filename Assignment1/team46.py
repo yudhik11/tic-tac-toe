dea0e2c00e6f667b5a08c446bc12d299ae69b2e8
yudhik11 <yudhik100@gmail.com>
2018-02-27 18:16:24 +0530

    hollow

--- Assignment1/team46.py
+++ Assignment1/team46.py
@@ -5,12 +5,11 @@ import copy
 class Team46:
     def __init__(self):
         self.INF = int(1e9)
-        self.maxmove = False
         self.timeLimit = datetime.timedelta(seconds = 2)
         self.begin = 0
         self.mark = 'x'
-        self.transpositionTable = {}
-        self.index = 0
+        self.transposition = {}
+        self.idx = 0
         self.moveOrder = dict()
         self.depth = 0
         self.val = [[],[],[],[]]
@@ -33,6 +32,12 @@ class Team46:
         self.mark = flag
         global board_copy
         board_copy = copy.deepcopy(board)
+        board_state_map = copy.deepcopy(board.board_status)
+        global store
+        store = []
+        for i in range(4):
+            for j in range(4):
+                store.append([ [board_state_map[4*i+k][4*j+l] for l in range(4)] for k in range(4)])
         curr = copy.deepcopy(old_move)
         self.begin = datetime.datetime.utcnow()
         ans = self.IDS(curr)
@@ -46,107 +51,108 @@ class Team46:
 
     def IDS(self, root):
         guess = 0
-        for depth in range(1, 7):
+        for depth in range(1, 6):
             self.depth = depth
-            self.transpositionTable = {}
-            guess, move = self.MTDF(root, guess, depth)
-            print(self.depth)
-            if self.check_time():
+            self.transposition = {}
+            # print "---------------------", depth, "-------------------"
+            guess, move = self.alpha_beta(True, root, -self.INF, self.INF, depth, False)
+            # print(self.depth)
+            if self.check_time() :
                 break
+            # if guess > 100000000:
+            #     return move
             saved_move = move
         return saved_move
 
-    def MTDF(self, root, guess, depth):
-        g = guess
-        upperbound = self.INF
-        lowerbound = -self.INF
-        while lowerbound < upperbound:
-            if g == lowerbound:
-                beta = g + 1
-            else:
-                beta = g
-            self.index = 0
-            self.maxmove = True
-            g, move = self.AlphaBetaWithMemory(root, beta - 1, beta, depth)
-            if self.check_time():
-                return g, move
-            if g < beta:
-                upperbound = g
-            else:
-                lowerbound = g
-        return g, move
+    # def MTDF(self, maxmove, root, guess, depth, bonusmove):
+    #     g = guess
+    #     upperbound = self.INF
+    #     lowerbound = -self.INF
+    #     while lowerbound < upperbound:
+    #         if g == lowerbound:
+    #             beta = g + 1
+    #         else:
+    #             beta = g
+    #         self.idx = 0
+    #         g, move = self.alpha_beta_mem(maxmove, root, beta - 1, beta, depth, bonusmove)
+    #         if self.check_time():
+    #             return g, move
+    #         if g < beta:
+    #             upperbound = g
+    #         else:
+    #             lowerbound = g
+    #     return g, move
 
-    def calcZobristHash(self, root):
+    def calc_zobrist(self, root):
         h = 0
         bs = board_copy.board_status
         for i in range(0, 16):
             for j in range(0, 16):
                 if bs[i][j] == 'x':
-                    h = h^self.zobrist[i][j][root[0]+1][root[1]+1][0]
+                    h = h ^ self.zobrist[i][j][root[0]+1][root[1]+1][0]
                 elif bs[i][j] == 'o':
-                    h = h^self.zobrist[i][j][root[0]+1][root[1]+1][1]
+                    h = h ^ self.zobrist[i][j][root[0]+1][root[1]+1][1]
         return h
 
-    def cantorPairing(self, a, b):
+    def cantor_pairing(self, a, b):
         return (a + b)*(a + b + 1)/2 + b
 
-    def getMarker(self, flag):
-        if flag == 'x':
-            return 'o'
-        return 'x'
+    def alpha_beta(self, maxmove, root, alpha, beta, depth, bonusmove):
+        board_hash = self.calc_zobrist(root)
+        # loweridx = self.cantor_pairing(board_hash, 1)
+        # upperidx = self.cantor_pairing(board_hash, 2)
 
-    def AlphaBetaWithMemory(self, root, alpha, beta, depth):
-        board_hash = self.calcZobristHash(root)
-        loweridx = self.cantorPairing(board_hash, 1)
-        upperidx = self.cantorPairing(board_hash, 2)
-
-        lower = -2*self.INF, root
-        upper = 2*self.INF, root
+        # lower = -2*self.INF, root
+        # upper = 2*self.INF, root
 
         # Transposition table lookup
-        if (loweridx in self.transpositionTable):
-            lower = self.transpositionTable[loweridx]
-            if (lower[0] >= beta):
-                return lower
+        # if (loweridx in self.transposition):
+        #     lower = self.transposition[loweridx]
+        #     if (lower[0] >= beta):
+        #         return lower
 
-        if (upperidx in self.transpositionTable):
-            upper = self.transpositionTable[upperidx]
-            if (upper[0] <= alpha):
-                return upper
+        # if (upperidx in self.transposition):
+        #     upper = self.transposition[upperidx]
+        #     if (upper[0] <= alpha):
+        #         return upper
 
         status = board_copy.find_terminal_state()
         if status[1] == "WON":
-            if self.maxmove:
+            if maxmove:
                 return -self.INF, root
             else:
                 return self.INF, root
     
         # Update values of alpha and beta based on values in Transposition Table
-        alpha = max(alpha, lower[0])
-        beta = min(beta, upper[0])
+        # alpha = max(alpha, lower[0])
+        # beta = min(beta, upper[0])
 
         # Get Unique Hash for moveOrder Table lookup
-        moveHash = self.cantorPairing(board_hash, self.index)
-        moveInfo = []
+        # moveHash = self.cantor_pairing(board_hash, self.idx)
+        # moveInfo = []
 
         # Reordered moves already present!
-        if moveHash in self.moveOrder:
-            children = []
-            children.extend(self.moveOrder[moveHash])
-            nSiblings = len(children)
+        # if moveHash in self.moveOrder:
+        #     children = []
+        #     children.extend(self.moveOrder[moveHash])
+        #     nSiblings = len(children)
 
-        else:
-            children = board_copy.find_valid_move_cells(root)
-            nSiblings = len(children)
+        # else:
+        children = board_copy.find_valid_move_cells(root)
+        random.shuffle(children)
+        # print children
+        nSiblings = len(children)
 
         if depth == 0 or nSiblings == 0:
             answer = root
             if self.check_time():
                 return 0, answer
-            g = self.heuristic(root, 0)
-
+            g = self.heuristic(maxmove, root, bonusmove)
+            # print root, g
+            return g, answer
+            
         # Node is a max node
-        elif self.maxmove:
+        if maxmove:
             g = -self.INF
             answer = children[0]
             # Return value if time up
@@ -154,10 +160,9 @@ class Team46:
                     return g, answer
 
             # Save original alpha value
-            a = alpha
+            # a = alpha
             i = 0
-            while ((g < beta) and (i < nSiblings)):
-                self.maxmove = False
+            while (i < nSiblings):
                 c = children[i]
 
                 #Retain current block state for later restoration
@@ -165,26 +170,37 @@ class Team46:
 
                 # Mark current node as taken by us for future reference
                 board_copy.update(root, c, self.mark)
-                self.index += 1
-
-                val, temp = self.AlphaBetaWithMemory(c, a, beta, depth-1)
+                # self.idx += 1
+                
+                #check for bonus move
+                flag_occupied = board_copy.block_status[c[0]/4][c[1]/4]
+                if flag_occupied == self.mark and not bonusmove :
+                    val, temp = self.alpha_beta(True, c, alpha, beta, depth-1, True)
+                else :
+                    # print c
+                    val, temp = self.alpha_beta(False, c, alpha, beta, depth-1, False)
 
                 # Revert Board state
                 board_copy.board_status[c[0]][c[1]] = '-'
                 board_copy.block_status[c[0]/4][c[1]/4] = blockVal
-                self.index -= 1
+                # self.idx -= 1
 
                 # Append returned values to moveInfo
-                temp = (val, c)
-                moveInfo.append(temp)
+                # temp = (val, c)
+                # moveInfo.append(temp)
 
                 if val > g:
                     g = val
                     answer = c
-
-                a = max(a, g)
+                    # print "maxmove: ", c, g
+                alpha = max(alpha, g)
+                if self.check_time():
+                    return g, answer
+                if alpha >= beta :
+                    break
+                # a = max(a, g)
                 i = i + 1
-            self.maxmove = True
+            maxmove = True
 
         # Node is a min node
         else:
@@ -194,65 +210,80 @@ class Team46:
             if self.check_time():
                 return g, answer
             # Save original beta value
-            b = beta
+            # b = beta
             i = 0
-            while ((g > alpha) and (i < nSiblings)):
-                self.maxmove = True
+            while (i < nSiblings):
                 c = children[i]
                 # Retain current block state for later restoration
                 blockVal = board_copy.block_status[c[0]/4][c[1]/4]
 
                 # Mark current node as taken by us for future reference
-                board_copy.update(root, c, self.getMarker(self.mark))
-                self.index += 1
+                oflag = self.mark
+                if oflag == 'x':
+                    oflag = 'o'
+                else :
+                    oflag = 'x'
+                board_copy.update(root, c, oflag)
+                # board_copy.print_board()
+                # self.idx += 1
 
-                val, temp = self.AlphaBetaWithMemory(c, alpha, b, depth-1)
-
+                #check for bonus move
+                flag_occupied = board_copy.block_status[c[0]/4][c[1]/4]
+                
+                if flag_occupied == oflag and not bonusmove :
+                    val, temp = self.alpha_beta(False , c, alpha, beta, depth-1, True)
+                else :
+                    val, temp = self.alpha_beta(True, c, alpha, beta, depth-1, False)
+                
                 # Revert Board state
                 board_copy.board_status[c[0]][c[1]] = '-'
                 board_copy.block_status[c[0]/4][c[1]/4] = blockVal
-                self.index -= 1
+                # self.idx -= 1
 
                 # Append returned values to moveInfo
-                temp = (val, c)
-                moveInfo.append(temp)
+                # temp = (val, c)
+                # moveInfo.append(temp)
 
                 if val < g:
                     g = val
                     answer = c
-
-                b = min(b, g)
+                beta = min(beta, g)
+                if self.check_time():
+                    return g, answer
+                if alpha >= beta :
+                    break
+                # b = min(b, g)
                 i = i + 1
-            self.maxmove = False
+            maxmove = False
 
         temp = []
 
-        if self.maxmove:
-            moveInfo = sorted(moveInfo, reverse = True)
-        else:
-            moveInfo = sorted(moveInfo)
+        # if maxmove:
+        #     moveInfo = sorted(moveInfo, reverse = True)
+        # else:
+        #     moveInfo = sorted(moveInfo)
 
-        for i in moveInfo:
-            temp.append(i[1])
-            children.remove(i[1])
+        # for i in moveInfo:
+        #     temp.append(i[1])
+        #     children.remove(i[1])
 
-        random.shuffle(children)
+        # random.shuffle(children)
 
-        self.moveOrder[moveHash] = []
-        self.moveOrder[moveHash].extend(temp)
-        self.moveOrder[moveHash].extend(children)
+        # self.moveOrder[moveHash] = []
+        # self.moveOrder[moveHash].extend(temp)
+        # self.moveOrder[moveHash].extend(children)
 
         # if(len(self.moveOrder[moveHash]) != nSiblings):
 
         # Traditional transposition table storing of bounds
         # ----------------------------------------------------#
         # Fail low result implies an upper bound
-        if g <= alpha:
-            self.transpositionTable[upperidx] = g, answer
+        # if g <= alpha:
+        #     self.transposition[upperidx] = g, answer
 
         # Fail high result implies a lower bound
-        if g >= beta:
-            self.transpositionTable[loweridx] = g, answer
+        # if g >= beta:
+        #     self.transposition[loweridx] = g, answer
 
         return g, answer
 
@@ -282,7 +313,13 @@ class Team46:
     def check_current_board_state(self, segment, flag, oflag):
         lheur = 0
         req_diamond = [(0,1), (0,2), (1,1), (1,2)]
-        val_arr = [ 50, 100, 225, 400]
+        if flag is self.mark:
+            val_arr = [ 0, 100, 325, 500]
+            val_arr_diamond = [ 0, 100, 350, 700]
+        else:
+            val_arr = [0, 50, 200, 600]
+            val_arr_diamond = [ 0, 50, 250, 600]
+            
         flag_diamond = 1
         if (segment[1][1] == segment[1][2] == oflag or \
             segment[1][1] == segment[2][1] == oflag or \
@@ -296,7 +333,7 @@ class Team46:
                 if val == -1:
                     lheur -= 50
                 else :
-                    lheur += val_arr[val]
+                    lheur += val_arr_diamond[val]
 
         for i in range(4):
             scounter_row = 0
@@ -314,16 +351,10 @@ class Team46:
                 elif segment[j][i] == oflag:
                     ocounter_column+=1
             if ocounter_row == 0:
-                lheur += flag_diamond * val_arr[scounter_row]
+                lheur += val_arr[scounter_row]
 
             if ocounter_column == 0:
-                lheur += flag_diamond * val_arr[scounter_column]
-
-            if scounter_row == 0:
-                lheur -= flag_diamond * val_arr[ocounter_row]
-
-            if scounter_column == 0:
-                lheur -= flag_diamond * val_arr[ocounter_column]
+                lheur += val_arr[scounter_column]
 
         return lheur
 
@@ -364,7 +395,7 @@ class Team46:
                     poss+=1
         return poss
 
-    def heuristic(self, move, bonusmove):
+    def heuristic(self, maxmove, move, bonusmove):
 
         currblockX = move[0]/4
         currblockY = move[1]/4
@@ -372,7 +403,7 @@ class Team46:
         nextblockY = move[1]%4
         bs = board_copy.block_status
         BS = board_copy.board_status
-        immwin = [500, -1000, -1200, -1300]
+        immwin = [500, -2500, -2600, -2700]
         flag = self.mark
         if flag == 'x':
             oflag = 'o'
@@ -381,27 +412,28 @@ class Team46:
 
         heur = 0
 
-        if self.depth == 1 and not bonusmove and bs[currblockX][currblockY] == flag:
-            children = board_copy.find_valid_move_cells((move[0], move[1]))
-            for child in children:
-                board_copy.update(move, child, flag)
-                heur = 10000 + self.heuristic(child, 1)
-                board_copy.board_status[child[0]][child[1]] = '-'
-                board_copy.block_status[child[0]/4][child[1]/4] = blockVal
+        # if self.depth == 1 and not bonusmove and bs[currblockX][currblockY] == flag:
+        #     children = board_copy.find_valid_move_cells((move[0], move[1]))
+        #     for child in children:
+        #         blockVal = board_copy.block_status[child[0]/4][child[1]/4]
+        #         board_copy.update(move, child, flag)
+        #         heur = 1000000000 + self.heuristic(child, 1)
+        #         board_copy.board_status[child[0]][child[1]] = '-'
+        #         board_copy.block_status[child[0]/4][child[1]/4] = blockVal
 
 
         # if self.depth == 1 and bs[currblockX][currblockY] == flag :
 
         if bs[nextblockX][nextblockY] != '-':
-            if self.maxmove:
-                heur += 1200/self.depth
+            if maxmove:
+                heur += 1500/self.depth
             else:
-                heur -= 1200/self.depth
+                heur -= 2000/self.depth
 
         if self.depth < 2:
             ret  = self.check_oppwin(nextblockX,nextblockY,oflag,flag)
-            if ret > len(immwin):
-                heur -= 1500
+            if ret >= len(immwin):
+                heur -= 3000
             else:
                 heur += immwin[ret]
 
@@ -409,9 +441,9 @@ class Team46:
         a, b = board_copy.find_terminal_state()
         if b == "WON":
             if a==flag:
-                heur += 100000
+                heur += 100000000
             elif a==oflag:
-                heur -= 100000
+                heur -= 100000000
         elif b == "DRAW":
             pts1=0
             pts2=0
@@ -421,22 +453,24 @@ class Team46:
                         pts1 += self.val[i][j]
                     if board_copy.block_status[i][j] == oflag:
                         pts2 += self.val[i][j]
-            heur+=(pts1-pts2)*20
+            heur+=(pts1-pts2)*25
         else:
             for i in range(4):
                 for j in range(4):
                     if bs[i][j] == flag:
-                        heur += self.val[i][j] *  120
+                        heur += self.val[i][j] * 120
                     elif bs[i][j] == oflag:
                         heur -= self.val[i][j] * 120
                     else:
                         temp = [ [BS[4*i+k][4*j+l] for l in range(4)] for k in range(4)]
-                        heur += self.check_current_board_state(temp, flag, oflag) * self.fac[i][j] /6
-                        heur -= self.check_current_board_state(temp, oflag, flag) * self.fac[i][j] /6
+                        scale = 1
+                        if store[i*4+j] != temp:
+                            scale = 1.5
+                        heur += self.check_current_board_state(temp, flag, oflag) * self.fac[i][j]/(2 * self.depth)
+                        heur -= self.check_current_board_state(temp, oflag, flag) * self.fac[i][j]/(2 * self.depth)
 
-            heur += self.check_current_board_state(bs, flag, oflag) * 4 
-            heur -= self.check_current_board_state(bs, oflag, flag) * 4
-
+            heur += self.check_current_board_state(bs, flag, oflag) 
+            heur -= self.check_current_board_state(bs, oflag, flag) 
         return heur
 
 
